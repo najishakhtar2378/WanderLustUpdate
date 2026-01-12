552ce23728ab67d70497bcc6b086d49ac76cfa8e
NAJISH  AKHTAR <166438726+najishakhtar2378@users.noreply.github.com>
2026-01-13 03:11:24 +0530

    Add async handling for host bookings route

--- routes/host.js
+++ routes/host.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middileware");
 const hostController = require("../controllers/host");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/dashboard", isLoggedIn, hostController.dashboard);
+router.get("/host/bookings", isLoggedIn, catchAsync(hostController.hostBookings));
 
 module.exports = router;
